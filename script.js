717d9a7e9038070bf19bd126b58a6a485252509e
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2024-12-29 22:29:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let data = document.getElementById("status");
+let buttonClicked = document.getElementById("enterBtn");
+
+buttonClicked.addEventListener('click', () => {
+	data.innerHTML="Entered Metaverse";
+})
